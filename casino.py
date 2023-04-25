6994ad5bfa0f8f6e8b667a715d062174caf03354
lukebluetiger <79377681+lukebluetiger@users.noreply.github.com>
2023-04-24 19:54:03 -0500

    small updates

--- casino.py
+++ casino.py
@@ -74,9 +74,13 @@ class Poker(commands.Cog):
             hand.append(card)
         return hand
 
-    def round(self, bet):
+    def bet(self, player, bet):  # simple money handling for each bet
+        self.money = user_money[player.id]
+        self.money -= bet 
         self.pot += bet
-        self.money -= bet  # simple money handling for each round
+        user_money[player.id] = self.money
+
+    def round(self): 
         round_card = self.cards[random.randint(
             0, len(self.cards)-1)]  # pick our round card
         self.table.append(round_card)
@@ -160,36 +164,34 @@ class Poker(commands.Cog):
                             self.game[player] = self.deal() # assign each player in dictionary a hand
                             await player.send("Your cards are `{}` and `{}`.".format(self.game[player][0], self.game[player][1])) # DM our cards so they are hidden
                         await ctx.send("Cards have been selected. Check your DMs for your hand.")
-                        while self.round_num < 3:
+                        for self.round_num in range(1,3):
+                            self.round()
                             for player in self.players:
-                                if player.id not in user_money:  # from our 'balance' command
-                                    user_money[player.id] = 2500.00
+                                if self.round_num == 1:
+                                    if player.id not in user_money:  # from our 'balance' command
+                                        user_money[player.id] = 2500.00
                                 self.money = user_money[player.id]
                                 await ctx.send("{} 's turn. What would you like to do?".format(player.mention))
                                 def check(m):
-                                    return m.author == player
+                                    return m.author == player and "bet" or "fold" in m # confirm this message is our player and they are playing
                                 msg = await client.wait_for('message', check=check)
 
                                 if "bet" in msg.content:
                                     bet_amount = int(msg.content.lstrip("!poker bet "))
                                     if bet_amount > self.money:  # prevent people from betting too much
-                                        await ctx.send("Bet higher than current balance, please try again.")
-                                        return
-                                    self.bets.append(bet_amount)
-                                    self.round(bet_amount)
+                                        return await ctx.send("Bet higher than current balance, please try again.")
+                                    self.bet(player, bet_amount)
                                     await ctx.send(f"${bet_amount:,.2f} added to the pot.")
                                     await ctx.send("Pot: `${}`".format(self.pot))
-                                    await ctx.send("Table: `{}`".format(self.table[0]))
+                                    await ctx.send(f"Table: `{'` `'.join(self.table)}`")
                                 
                                 elif "fold" in msg.content:
-                                    print(int(self.players.index(player)))
                                     await ctx.send(f"Folded. Your current balance is ${self.money:,.2f}.")
                                     user_money[ctx.author.id] = self.money
                                     self.players.remove(player)
                                     if len(self.players) == 1:
                                         await self.winner(self.players[0])
-                                
-                                self.round_num += 1
+
                 
                             
 
